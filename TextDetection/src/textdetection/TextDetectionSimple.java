ed3e90260ceae291177ccb35c0d3efc7f3866f52
User <User@DESKTOP-0PM2KEF>
2023-10-09 01:02:26 -0500

    Implementación Completa

---
+++ TextDetection/src/textdetection/TextDetectionSimple.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package textdetection;
+
+/**
+ *
+ * @author User
+ */
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TextDetectionSimple {
+
+    public static void main(String[] args) {
+        Tesseract tesseract = new Tesseract();
+        try {
+
+            tesseract.setDatapath("src\\main\\resources\\tessdata");
+
+            // the path of your tess data folder 
+            // inside the extracted file 
+            String text
+                    = tesseract.doOCR(new File("src\\main\\resources\\images\\Image.png"));
+
+            // path of your image file 
+            System.out.print(text);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
